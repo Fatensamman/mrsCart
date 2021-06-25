4c5f2c7d5373b7759db10e561190de085327728e
Fatensamman <arch.fatensamman@gmail.com>
2021-06-25 21:58:20 +0300

    cleardb

--- database/db.js
+++ database/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 require('dotenv').config()
 
 const client = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    host:us-cdbr-east-04.cleardb.com,
+    user: b78fc5bf03b867,
+    password: da0844b7,
+    database: heroku_ca738264f38a57a,
 });
 
 client.connect((err) => {
